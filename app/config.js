463d7f31237f7f906ac7e370a1186c35075c4910
AnnaShmith <59558379+AnnaShmith@users.noreply.github.com>
2020-04-09 11:32:53 +0100

    Covid Work start

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Applying for a Juggling License',
+  serviceName: 'Keyworker Self-referral Test',
 
   // Default port that prototype runs on
   port: '3000',
